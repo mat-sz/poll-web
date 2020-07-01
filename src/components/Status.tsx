c2af81ac0366acfa242d8dc4c6300c93cd836f69
Mat Sz <contact@matsz.dev>
2020-07-01 18:04:39 +0200

    added a home link

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { StateType } from '../reducers';
 import { title } from '../config';
@@ -9,7 +10,9 @@ const Status: React.FC = () => {
 
   return (
     <>
-      <h1>{title}</h1>
+      <Link to="/">
+        <h1>{title}</h1>
+      </Link>
       {!connected ? <div className="status">Connecting...</div> : null}
     </>
   );
